1aed8a86b2b20d1107bc09dff70e2d1fb76b72ba
fantom030983 <ihorkavun1234@gmail.com>
2023-09-27 15:01:23 +0300

    average

---
+++ js/average.js
@@ -0,0 +1,5 @@
+var numOne = parseInt(prompt('Введіть перше число:'));
+var numTwo = parseInt(prompt('Введіть друге число:'));
+var numThree = parseInt(prompt('Введіть третє число:'));
+
+alert (Number(numOne + numTwo + numThree)/3);
